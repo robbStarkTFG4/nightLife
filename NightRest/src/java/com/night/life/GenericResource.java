e7755a0c90d419316b640cb9764645dc282bd311
robb <marco_417_@hotmail.com>
2016-04-05 22:35:42 -0700

    initial commit

---
+++ NightRest/src/java/com/night/life/GenericResource.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.night.life;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ * http://nightlife2-contactres.rhcloud.com/NightRest/webresources/generic
+ *
+ * @author marcoisaac
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.night.life.GenericResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+
+        return "hola crayola!!!!";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     *
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
